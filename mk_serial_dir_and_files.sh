4a94880beadc3200015cc029df1d7b325b969077
Black Dragon(白い巨匠) <48268998+bzgwhite@users.noreply.github.com>
2020-08-31 12:02:11 +0900

    Update mk_serial_dir_and_files.sh

--- mk_serial_dir_and_files.sh
+++ mk_serial_dir_and_files.sh
@@ -2,5 +2,5 @@
 
 touch File{1..10}.txt
 mkdir -p tst/test{1..10}
-find ./tst -type d -name "test*" -maxdepth 1 | xargs -n 1 cp File* 
+find ./tst -maxdepth 1 -type d -name "test*" | xargs -n 1 cp File* 
 
